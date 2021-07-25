01d637510b3845bed55ec4919edb1cf2a421c551
alanpapalia <alanpapalia@gmail.com>
2021-07-25 17:53:25 -0400

    working on incorporating movements into the robot - currently having issue with the exponential map constructor for SE(2)

--- examples/single_robot_random_trajectory.py
+++ examples/single_robot_random_trajectory.py
@@ -11,7 +11,7 @@ from manhattan.simulator.simulator import ManhattanSimulator
 sim_args = ManhattanSimulator.SimulationParams(
     grid_shape=(20, 20),
     row_corner_number=2,
-    column_corner_number=5,
+    column_corner_number=2,
     cell_scale=1.0,
     range_sensing_prob=0.5,
     ambiguous_data_association_prob=0.1,
@@ -20,7 +20,15 @@ sim_args = ManhattanSimulator.SimulationParams(
     loop_closure_radius=3.0,
 )
 sim = ManhattanSimulator(sim_args)
-sim.add_robot()
-sim.add_beacon()
+
+num_robots = 3
+num_beacons = 2
+sim.add_robots(num_robots)
+sim.add_beacons(num_beacons)
+
+num_timesteps = 10
+for _ in range(num_timesteps):
+    sim.move_robots_randomly()
+
+sim.print_simulator_state()
 sim.plot_current_state(show_grid=True)
-print(sim)
