273f40c3f558cce5d8d7e14e287f9039ab23e35d
alanpapalia <alanpapalia@gmail.com>
2021-12-14 09:04:03 -0500

    small cleanup

--- examples/example.py
+++ examples/example.py
@@ -10,31 +10,31 @@ import numpy as np
 
 show_animation = False
 
-for num_timesteps in [500]:
-    for pos_stddev in [1e-1]:
-        for theta_stddev in [1e-2]:
-            for dist_stddev in [1e-3]:
+for num_timesteps in [700]:
+    for pos_stddev in [1e-2]:
+        for theta_stddev in [1e-3]:
+            for dist_stddev in [5e-1]:
                 for num_beacons in [1]:
-                    for loop_prob in [0.05]:
-                        for grid_len in [10]:
+                    for range_prob in [0.6]:
+                        for grid_len in [50]:
                             print()
                             for i in range(0, 1):
                                 sim_args = SimulationParams(
-                                    num_robots=3,
+                                    num_robots=6,
                                     num_beacons=num_beacons,
                                     grid_shape=(grid_len, grid_len),
                                     y_steps_to_intersection=2,
-                                    x_steps_to_intersection=2,
+                                    x_steps_to_intersection=5,
                                     cell_scale=1.0,
-                                    range_sensing_prob=0.5,
-                                    range_sensing_radius=999.0,
+                                    range_sensing_prob=range_prob,
+                                    range_sensing_radius=10.0,
                                     false_range_data_association_prob=0.0,
                                     outlier_prob=0.0,
                                     max_num_loop_closures=9999999,
-                                    loop_closure_prob=loop_prob,
-                                    loop_closure_radius=10.0,
+                                    loop_closure_prob=0.05,
+                                    loop_closure_radius=20.0,
                                     false_loop_closure_prob=0.0,
-                                    range_stddev=1e-1,
+                                    range_stddev=dist_stddev,
                                     odom_x_stddev=pos_stddev,
                                     odom_y_stddev=pos_stddev,
                                     odom_theta_stddev=theta_stddev,
@@ -42,7 +42,7 @@ for num_timesteps in [500]:
                                     loop_y_stddev=pos_stddev,
                                     loop_theta_stddev=theta_stddev,
                                     debug_mode=False,
-                                    seed_num=i,
+                                    seed_num=(i + 1) * 9999,
                                     groundtruth_measurements=False,
                                     # no_loop_pose_idx=[0, 1, 2],
                                     # exclude_last_n_poses_for_loop_closure=2
@@ -71,10 +71,11 @@ for num_timesteps in [500]:
                                         f"{num_timesteps}_timesteps",
                                         f"{int(pos_stddev*1000)}_pos_stddev",
                                         f"{int(theta_stddev*1000)}_rot_stddev",
-                                        f"{int(loop_prob*100)}_loop_prob",
+                                        f"{int(range_prob*100)}_range_prob",
                                         f"{num_beacons}_beacons",
                                         f"{grid_len}_grid",
                                         f"seed_{sim_args.seed_num}",
                                     )
                                 )
                                 sim.save_simulation_data(data_dir, format="pickle")
+                                print(f"Saved data to {data_dir}")
